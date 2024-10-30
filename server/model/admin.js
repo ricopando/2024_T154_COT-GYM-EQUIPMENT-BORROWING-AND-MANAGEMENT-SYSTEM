cfcba4372227206e91d506b74df6cc730fd76867
nonim <115530921+BarquillaBenz@users.noreply.github.com>
2024-10-30 10:15:16 +0800

    Delete server directory

--- server/model/admin.js
+++
@@ -1,9 +0,0 @@
-const Admin = [
-    {
-        "name": "Alice Johnson",
-        "email": "alice.johnson@example.com",
-        "role": "admin",
-        "personnelId": "60d5f8a0c8c45f3f2456e600"
-    },
-]
-export default Admin;
